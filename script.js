7468f4e5698ca52ee1be24de18a9da0da1c9d977
vasilisk7258 <ertybasevk@gmail.com>
2025-06-09 16:57:35 +0300

    d58

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const SCALE_LIMITS = { min: 0.3, max: 5 };
 
 // Инициализация
 function init() {
-  console.log("Initializing..57");
+  console.log("Initializing..58");
   console.log("Initializing AR Scene");
   // 1. Настройка Three.js сцены
   scene = new THREE.Scene();
@@ -47,7 +47,7 @@ function init() {
   camera.position.z = 5;
 
   // 3. Освещение
-  const ambientLight = new THREE.AmbientLight(0xffffff, 10);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1);
   scene.add(ambientLight);
 
   // 4. Загрузка 3D-модели
